aa7c1f31b45b9b228d8b0c71bd520e05e4156c8d
Viktor <viktor.sn404@gmail.com>
2024-05-02 16:03:12 +0200

    Moving the tool to a new repo.

---
+++ TransactionsGrouper.cs
@@ -0,0 +1,76 @@
+﻿using System.Text;
+
+namespace TransactionsImport
+{
+	internal class TransactionsGrouper
+	{
+		public IEnumerable<AccountTransaction> Group(IEnumerable<AccountTransaction> transactions)
+		{
+			List<AccountTransaction> unclassifiedTransactions = new List<AccountTransaction>();
+			List<AccountTransaction> classifiedTransactions = new List<AccountTransaction>();
+            foreach (AccountTransaction tran in transactions)
+			{
+				if (tran.Category == null && tran.Budget == null)
+                    unclassifiedTransactions.Add(tran);
+				else
+					classifiedTransactions.Add(tran);
+			}
+
+			var grouppedTransactions = unclassifiedTransactions
+				.GroupBy(t => (t.Date, t.Category, t.Account, t.Budget, t.Description),
+					CreateGroupTransaction)
+				.Union(classifiedTransactions
+					.GroupBy(t => (t.Date, t.Category, t.Account, t.Budget, t.FriendlyDescription),
+					CreateGroupTransaction));
+
+			foreach (AccountTransaction tran in grouppedTransactions)
+			{
+				yield return tran;
+			}
+		}
+
+		private AccountTransaction CreateGroupTransaction(
+			(DateTime Date, string? Category, string? Account, string? Budget, string? FriendlyDescription) key,
+			IEnumerable<AccountTransaction> transactionsGroup)
+		{
+			if (transactionsGroup.Count() == 1)
+			{
+				return transactionsGroup.First();
+			}
+
+			decimal amount = 0m;
+			StringBuilder? descriptionBuilder = null;
+			foreach (var tran in transactionsGroup)
+			{
+				amount += tran.Debit - tran.Credit;
+
+				if (tran.Description != null)
+				{
+					if (descriptionBuilder == null)
+					{
+						descriptionBuilder = new StringBuilder(tran.Description);
+					}
+					else
+					{
+						descriptionBuilder.Append("; ");
+						descriptionBuilder.Append(tran.Description);
+					}
+				}
+			}
+
+			return new AccountTransaction
+			{
+				Date = key.Date,
+				Debit = amount > 0m ? amount : 0m,
+				Credit = amount < 0m ? -amount : 0m,
+				CurrentAmount = null,
+				Description = descriptionBuilder != null ? descriptionBuilder.ToString() : null,
+
+				Account = key.Account,
+				Budget = key.Budget,
+				Category = key.Category,
+				FriendlyDescription = key.FriendlyDescription
+			};
+		}
+	}
+}
---
+++ TransactionsReader.cs
@@ -0,0 +1,64 @@
+﻿using System.Globalization;
+
+namespace TransactionsImport
+{
+	internal class TransactionsReader
+	{
+        private readonly string _inputDecimalSeparator;
+
+        public string FilePath { get; }
+
+		public TransactionsReader(string csvFilePath, string inputDecimalSeparator)
+		{
+            if (inputDecimalSeparator.Length > 1)
+                throw new ArgumentException($"{nameof(inputDecimalSeparator)} '{inputDecimalSeparator}' should be a single character.");
+
+            FilePath = csvFilePath;
+            _inputDecimalSeparator = inputDecimalSeparator;
+        }
+
+		public IEnumerable<AccountTransaction> Read()
+		{
+			return File.ReadLines(FilePath).Select(ParseLine);
+		}
+
+		private AccountTransaction ParseLine(string csvLine)
+		{
+			var columns = csvLine.Trim('\"').Split("\",\"");
+			if (columns.Length != 6)
+			{
+				var ex = new ArgumentException("The CSV line invalid format. It is expected to contain 6 columns.", nameof(csvLine));
+				ex.Data.Add("Expected format", "\"Value date\",\"Processing date\",\"Debit\",\"Credit\",\"Amount\",\"Description\"");
+				ex.Data.Add(nameof(csvLine), csvLine);
+				throw ex;
+			}
+
+			CultureInfo culture = (CultureInfo)CultureInfo.InvariantCulture.Clone();
+            culture.NumberFormat.NumberGroupSeparator = "";
+            culture.NumberFormat.NumberDecimalSeparator = _inputDecimalSeparator;
+
+            var tran = new AccountTransaction();
+			tran.Date = DateTime.ParseExact(columns[1], "dd.MM.yyyy", culture);
+			tran.Debit = !string.IsNullOrEmpty(columns[2]) ? ToDecimal(columns[2], culture) : 0m;
+			tran.Credit = !string.IsNullOrEmpty(columns[3]) ? ToDecimal(columns[3], culture) : 0m;
+			tran.CurrentAmount = !string.IsNullOrEmpty(columns[4]) ? ToDecimal(columns[4], culture) : null;
+			tran.Description = columns[5].Trim();
+			return tran;
+		}
+
+        private decimal ToDecimal(string value, CultureInfo culture)
+		{
+			string cleanValue = new string(value.Where(c => Char.IsNumber(c) || c == _inputDecimalSeparator[0]).ToArray());
+			decimal result;
+			if (Decimal.TryParse(cleanValue, NumberStyles.Any, culture, out result))
+			{
+				return result;
+			}
+			else
+			{
+				throw new FormatException(
+					$"Can not convert {value} string to the decimal value with a {_inputDecimalSeparator} separator.");
+			}
+		}
+    }
+}
